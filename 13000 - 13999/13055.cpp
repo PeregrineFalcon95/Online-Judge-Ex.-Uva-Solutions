23962d24a18b131f10dd371de8b942de3c5739e0
Amit Sarker <amitcse1995@gmail.com>
2020-10-10 09:41:14 +0600

    Solved 13055

---
+++ 13000 - 13999/13055.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define mod ( ll  ) ( 1e9 + 7 )  //( 1e9 + 7 )
+#define ll long long int
+#define inf ( ll  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+int n, m, t, ans , cases;
+string s , s2;
+int ar [ mxs ] ;
+void brainfuck();
+int main()
+{
+    //freopen("tour.in","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    //cin>>t;
+    t = 1;
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    stack < string > stk;
+    cin>>n;
+    while( n-- )
+    {
+        cin>>s;
+        if ( s == "Sleep" )
+        {
+            cin>>s2;
+            stk.push( s2 );
+        }
+        else if ( s == "Kick" )
+        {
+            if ( !stk.empty() )stk.pop();
+        }
+        else
+        {
+            if ( !stk.empty() )cout<<stk.top()<<'\n';
+            else cout<<"Not in a dream\n";
+        }
+    }
+}
