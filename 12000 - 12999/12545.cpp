6a98ea3940f9a3c37d082f79bb9f00655f1ad202
Amit Sarker <amitcse1995@gmail.com>
2020-11-12 06:54:07 +0600

    Solved 12545

---
+++ 12000 - 12999/12545.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) ( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( ll  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+int n, m, t, ans, cases;
+string s , s2;
+int ar [ mxs ] ;
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    cin>>t;
+    //t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    cin>>s>>s2;
+    n = s.size();
+    int zero = 0 , one = 0 , qZero = 0 , qOne = 0;
+    ans = 0;
+    for ( int i = 0 ; i < n; i++ )
+    {
+        if ( s [ i ] == '?' )
+        {
+            if ( s2 [ i ] == '0' )qZero++;
+            else qOne++;
+        }
+        else if ( s [ i ] != s2 [ i ] )
+        {
+            if ( s [ i ] == '0' )zero++;
+            else one++;
+        }
+    }
+    ans = min( zero , one );
+    zero -= ans;
+    one -= ans;
+    if ( zero )
+    {
+        ans += zero + qZero + qOne;
+    }
+    else
+    {
+        int x = min( one , qOne );
+        ans += x + qOne + qZero;
+        one -= x;
+        qOne -= x;
+        if ( one )
+        {
+            cout<<"Case "<<++cases<<": -1"<<'\n';
+            return;
+        }
+    }
+    cout<<"Case "<<++cases<<": "<<ans<<'\n';
+}
+
