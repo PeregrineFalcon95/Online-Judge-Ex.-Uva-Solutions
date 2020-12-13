bb0cc5af03a6b311ebbeff8d5d73a76de1547c6c
Amit Sarker <amitcse1995@gmail.com>
2020-12-14 05:11:43 +0600

    Solved 12955

---
+++ 12000 - 12999/UVA 12955 Factorial.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) ( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+int n , m, t, ans, cases , total ;
+string s , s2;
+int dp [ mxs ] , fact [ 12 ];
+int rec ( int idx )
+{
+    if ( !idx )return 0;
+    if ( dp [ idx ] != -1 )return dp [ idx ];
+    dp [ idx ] = inf;
+    for ( int i = 1 ; i <= 8; i++ )
+        if ( idx - fact [ i ] >= 0 )dp [ idx ] = min( dp [ idx ] , rec( idx - fact [ i ] ) + 1 );
+
+    return dp [ idx ];
+}
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    //cin>>t;
+    t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    fact [ 1 ] = 1;
+    for ( int i = 2 ; i <= 9; i++ )fact [ i ] = fact [ i - 1 ] * i;
+    memset( dp , -1, sizeof dp );
+
+    while( cin>>n )cout<<rec(n)<<"\n";
+}
+
