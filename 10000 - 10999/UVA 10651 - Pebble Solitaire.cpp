08dfc9522488736ec12addf0847ac94fa29de7f6
Amit Sarker <amitcse1995@gmail.com>
2020-12-18 06:56:10 +0600

    Solved 10651

---
+++ 10000 - 10999/UVA 10651 - Pebble Solitaire.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) ( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+int n , m, t, ans, cases ;
+string s , s2;
+int backtrack()
+{
+    int mi = inf;
+    for ( int i = 0 ; i + 2 < 12; i++ )
+    {
+        if ( s.substr(i,3) == "-oo" )
+        {
+            s [ i ] = 'o';s [ i + 1 ] = '-';s [ i + 2 ] = '-';
+            mi = min( mi , backtrack() );
+            s [ i ] = '-';s [ i + 1 ] = 'o';s [ i + 2 ] = 'o';
+        }
+        else if ( s.substr(i,3) == "oo-" )
+        {
+            s [ i ] = '-';s [ i + 1 ] = '-';s [ i + 2 ] = 'o';
+            mi = min( mi, backtrack() );
+            s [ i ] = 'o';s [ i + 1 ] = 'o';s [ i + 2 ] = '-';
+        }
+    }
+    if ( mi == inf )
+    {
+        mi = 0;
+        for ( int i = 0 ; i < 12; i++ )
+            if ( s [ i ] == 'o' )mi++;
+    }
+    return mi;
+}
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    cin>>t;
+    //t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    cin>>s;
+    cout<<backtrack()<<"\n";
+}
+
