6cec0526a44b5480d7f61b2bdcb0cd2f4acd0f72
Amit Sarker <amitcse1995@gmail.com>
2020-12-20 08:52:22 +0600

    Solved 12621

---
+++ 12000 - 12999/UVA 12621 - On a Diet.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) ( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+int n , m, t, ans, cases ;
+string s , s2;
+ll ar [ 101 ] , dp [ 110 ] [ 255 ];
+int rec ( int idx , int val )
+{
+    if ( val >= m )return 0;
+    if ( idx > n )
+    {
+        if ( val >= m )return 0;
+        return inf;
+    }
+    ll &p = dp [ idx ] [ val ];
+    p = rec( idx + 1 , val );
+    p = min( p , rec( idx + 1 , val + ar [ idx ] ) + ar [ idx ] );
+    return p;
+}
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    cin>>t;
+    //t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    cin>>m>>n;
+    m /= 10;
+    for ( int i = 1 ; i <= n; i++ )
+    {
+        cin>>ar [ i ];
+        ar [ i ] /= 10;
+    }
+    memset( dp , -1, sizeof dp );
+    ll x = rec( 1 , 0 );
+    if ( x >= inf )cout<<"NO SOLUTION\n";
+    else cout<<x<<"0\n";
+}
