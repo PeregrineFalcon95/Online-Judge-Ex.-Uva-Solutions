8f48c702417590652689ce82f25cc5fac0fb1e01
Amit Sarker <amitcse1995@gmail.com>
2022-10-21 05:27:45 +0600

    resolve 336 and 762

--- 100 - 999/336.cpp
+++ 100 - 999/336.cpp
@@ -1,58 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int a,b,c,edges,cases=0;
-    while(scanf("%d",&edges)==1&&edges!=0)
-    {
-        map<int,vector<int>>graph;
-        set<int>nodes;
-        set<int>::iterator it=nodes.begin();
-        for(int i=0; i<edges; i++)
-        {
-            scanf("%d%d",&a,&b);
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-            nodes.insert(it,a);
-            nodes.insert(it,b);
-        }
-        int number_of_nodes=nodes.size();
-        c=0;
-        vector<int>query[2];
-        while(scanf("%d%d",&a,&b)==2&&(a!=0||b!=0))
-        {
-            c++;
-            query[0].push_back(a);
-            query[1].push_back(b);
-        }
-        int size_query=query[0].size();
-        for(int i=0; i<c; i++)
-        {
-            map<int,int>distance;
-            queue<int>qu;
-            int y=1;
-            qu.push(query[0][i]);
-            distance[query[0][i]]=0;
-            while(!qu.empty())
-            {
-                int front_=qu.front();
-                qu.pop();
-                int sz=graph[front_].size();
-                for(int j=0; j<sz; j++)
-                {
-                    int next_distance=distance[front_]+1;
-                    if(next_distance>query[1][i])break;
-                    else if(distance.find(graph[front_][j])==distance.end())
-                    {
-                        y++;
-                        distance[graph[front_][j]]=next_distance;
-                        qu.push(graph[front_][j]);
-                    }
+#define mod 1000000007
+#define ll long long int
+#define inf ( int )( 1e9 + 10 )
+#define mxs (int)( 1e6 + 10 )
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie( NULL);
+//#define md int mid = ( l + r )>>1;
+int dx[]={+1,-1,+0,+0};///Four Directions
+int dy[]={+0,+0,+1,-1};///Four directions
+// int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+// int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+int t, cases;
+
+
+class solution{
+private:
+    int numOfReachable(int source, int range, unordered_map < int , vector < int > >& graph){
+        queue < int > qu;
+        unordered_map < int, int > level;
+        qu.push(source);
+        level [ source ] = 0;
+        int nodeCount = 0;
+        while( !qu.empty() ){
+            source = qu.front();
+            qu.pop();
+            nodeCount++;
+            auto it = graph [ source ];
+            int sz = it.size();
+            for ( int i = 0; i < sz; i++ ){
+                int child = it [ i ];
+                if ( level.find( child ) == level.end() && level [ source ] < range ){
+                    level [ child ] = level [ source ] + 1;
+                    qu.push( child );
                 }
             }
-            int ans=number_of_nodes-y;
-            printf("Case %d: %d nodes not reachable from node %d with TTL = %d.\n",++cases,ans,query[0][i],query[1][i]);
+        }
+        return nodeCount;
+    }
+public:
+    void brainfuck(){
+        int numOfEdges;
+        while( cin>>numOfEdges && numOfEdges ){
+            unordered_map < int , vector < int > > graph;
+            int a, b;
+            while( numOfEdges-- ){
+                cin>>a>>b;
+                graph [ a ].push_back( b );
+                graph [ b ].push_back( a );
+            }
+
+            while( cin>>a>>b && (a || b) ){
+                int totalNodes = graph.size();
+                if ( graph.find(a) == graph.end() )totalNodes++;
+                cout<<"Case "<<++cases<<": "<<totalNodes - numOfReachable(a,b,graph)<<" nodes not reachable from node "<<a<<" with TTL = "<<b<<".\n";
+            }
         }
     }
+};
+
+solution solve;
+int main(){
+    fast
+
+    //cin>>t;
+    t = 1;
+    while( t-- )solve.brainfuck();
+    
     return 0;
 }
+
--- 100 - 999/762.cpp
+++ 100 - 999/762.cpp
@@ -1,148 +1,99 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    string value;
-    int m=0;
-    int n;
-    while(scanf("%d",&n)==1)
-    {
-        if(m)printf("\n");
-        m++;
-        cin.ignore();
-        int source,target;
-        vector < int > graph[(n*2)+5];
-        map < int , string > compress;
-        int flag=1;
-        for(int i=0; i<n; i++)
-        {
-            string ss;
-            getline(cin,ss);
-            string token="";
-            stringstream sss(ss);
-            int a=0,b=0;
-            int ff=0;
-            while(sss>>token)
-            {
-                int c=0;
-                if(ff==2)break;
-                for(map < int, string >::iterator it=compress.begin(); it!=compress.end(); it++)
-                {
-                    if(it->second == token)
-                    {
-                        c=1;
-                        if(!ff)
-                        {
-                            ff=1;
-                            a=it->first;
-                        }
-                        else
-                        {
-                            ff=2;
-                            b=it->first;
-                        }
-                    }
-                }
-                if(!c)
-                {
-                    if(!ff)
-                    {
-                        ff=1;
-                        a=flag++;
-                    }
-                    else if(ff)
-                    {
-                        ff=2;
-                        b=flag++;
-                    }
-                }
-                if(ff==1)compress[a]=token;
-                else if(ff==2)compress[b]=token;
-            }
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-        string s;
-        getline(cin,s);
-        stringstream ss(s);
-        string token="";
-        int ff=0;
-        while(ss>>token)
-        {
-            if(ff==2)break;
-            int a;
-            for(map < int, string >::iterator it=compress.begin(); it!=compress.end(); it++)
-            {
-                if(it->second == token)
-                {
-                    if(!ff)
-                    {
-                       a=it->first;
-                       ff=1;
-                    }
-                    else if(ff==1)
-                    {
-                        ff=2;
-                        a=it->first;
-                    }
-                }
-            }
-            if(ff==1)source=a;
-            else if(ff==2)target=a;
-        }
-        bool visited[n+10];
-        memset(visited, 0, sizeof visited);
-        map < int, int > back_edges;
-        back_edges[source]=-1;
+#define mod 1000000007
+#define ll long long int
+#define inf ( int )( 1e9 + 10 )
+#define mxs (int)( 1e6 + 10 )
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie( NULL);
+//#define md int mid = ( l + r )>>1;
+int dx[]={+1,-1,+0,+0};///Four Directions
+int dy[]={+0,+0,+1,-1};///Four directions
+// int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+// int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+int t, cases;
+
+
+class solution{
+private:
+    void findPath(int source, int target, int total, vector < vector < int > >& graph, vector < int >& path){
         queue < int > qu;
-        qu.push(source);
-        visited[source]=1;
-        flag=0;
-        while(!qu.empty())
-        {
-            int front_=qu.front();
+        vector < int > parent ( total + 1, 0);
+        qu.push( source );
+        parent [ source ] = source;
+        while( !qu.empty() ){
+            int node = qu.front();
             qu.pop();
-            int sz=graph[front_].size();
-            for(int i=0; i!=sz; i++)
-            {
-                int fff=graph[front_][i];
-                if(!visited[fff])
-                {
-                    qu.push(fff);
-                    visited[fff]=1;
-                    back_edges[fff]=front_;
-                    if(fff==target)
-                    {
-                        flag=1;
-                        break;
-                    }
+            int sz = graph [ node ].size();
+            if ( node == target )break;
+            for ( int i = 0; i < sz; i++ ){
+                int child = graph [ node ] [ i ];
+                if ( !parent [ child ] ){
+                    parent [ child ] = node;
+                    qu.push( child );
                 }
             }
-            if(flag)break;
         }
-        if(!flag)
-        {
-            printf("No route\n");
-            continue;
+        if ( !parent [ target ] )return;
+        for ( ; target != source ; target = parent [ target ] )
+            path.push_back( target );
+        path.push_back( target );
+    }
+    int compressStr(string& s, int& marker, map < string , int >& strToNum, map < int , string >& numToStr){
+        if ( strToNum.find( s ) == strToNum.end() ){
+            strToNum [ s ] = ++marker;
+            numToStr [ marker ] = s;
+            return marker;
         }
-        vector < int > temp;
-        int pre=target;
-        while(1)
-        {
-            temp.push_back(pre);
-            pre=back_edges[pre];
-            if(pre==-1)break;
-        }
-        string kk=compress[temp.back()];
-        reverse(temp.begin(),temp.end());
-        int sz=temp.size();
-        for(int i=1; i!=sz; i++)
-        {
-            cout<<kk<<" ";
-            cout<<compress[temp[i]]<<endl;
-            kk="";
-            kk=compress[temp[i]];
+        else return strToNum [ s ];
+    }
+public:
+    void brainfuck(){
+        int numOfEdges;
+        while( cin>>numOfEdges ){
+            if ( cases++ )cout<<"\n";
+            string s, s2;
+            map < string , int > strToNum;
+            map < int , string > numToStr;
+            vector < vector < int > > graph ( numOfEdges+numOfEdges+2, vector < int > ());
+            vector < int > path;
+
+            int marker = 0;
+            while( numOfEdges-- ){
+                cin>>s>>s2;
+                int a, b;
+                a = compressStr(s, marker, strToNum, numToStr);
+                b = compressStr(s2, marker, strToNum, numToStr);
+                graph [ a ].push_back( b );
+                graph [ b ].push_back( a );
+            }
+            cin>>s>>s2;
+            int source = compressStr(s, marker, strToNum, numToStr);
+            int target = compressStr(s2, marker, strToNum, numToStr);
+            findPath(source, target, marker, graph, path);
+
+            int n = path.size();
+            if ( !n ){
+                cout<<"No route\n";
+                continue;
+            }
+
+            for ( int i = n - 1; i > 0; i-- )
+                cout<<numToStr [ path [ i ] ]<<" "<<numToStr [ path [ i - 1 ] ]<<"\n";
         }
     }
+};
+
+solution solve;
+int main(){
+    fast
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    //cin>>t;
+    t = 1;
+    while( t-- )solve.brainfuck();
+    
     return 0;
 }
+
